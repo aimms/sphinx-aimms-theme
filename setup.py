3a9805fa7b3c2d4e8ae4a1d05fcce0ff7b8fd10d
ArthurH <arthur.dherbemont@aimms.com>
2019-10-04 12:22:15 +0200

    pushed new version to Pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 setup(
     
     name = "sphinx_aimms_theme",
-    version = '0.0.1',
+    version = '0.0.3',
     license = "MIT",
     packages= ['sphinx_aimms_theme'],
     url = "https://gitlab.com/ArthurdHerbemont/sphinx-aimms-theme",
