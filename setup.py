a25c8849a7d3e0be7e5bcf8fb919a2b7e3590ea7
Mohan <mohan.chiriki@aimms.com>
2021-03-31 16:56:28 -0700

    v_0_1_27 formatting changes to help and feedback

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 setup(
     
     name = "sphinx_aimms_theme",
-    version = '0.1.24',
+    version = '0.1.27',
     license = "MIT",
     packages= ['sphinx_aimms_theme'],
     url = "https://gitlab.com/ArthurdHerbemont/sphinx-aimms-theme",
