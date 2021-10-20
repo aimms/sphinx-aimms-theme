69fee7f6598b484aefd826e7e610d1f4ac369fe2
ArthurH <arthur.dherbemont@aimms.com>
2021-10-20 19:45:05 +0200

    fixed sidebar inconsistent shift

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 setup(
     
     name = "sphinx_aimms_theme",
-    version = '0.1.34',
+    version = '0.1.35',
     license = "MIT",
     packages= ['sphinx_aimms_theme'],
     url = "https://gitlab.com/ArthurdHerbemont/sphinx-aimms-theme",
