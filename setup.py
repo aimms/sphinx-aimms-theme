5515bb90af4c36040861661ad3852c4d0c45090d
Luis Pinto <luis.pinto@aimms.com>
2022-03-10 11:37:34 +0100

    updates version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 setup(
     
     name = "sphinx_aimms_theme",
-    version = '0.1.40',
+    version = '0.1.41',
     license = "MIT",
     packages= ['sphinx_aimms_theme'],
     url = "https://github.com/aimms/sphinx-aimms-theme",
