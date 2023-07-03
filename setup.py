34ddb6fbe3513cca4f0adbb3ff4c819ef162e14c
Luis Pinto <luis.pinto@aimms.com>
2023-07-03 23:11:02 +0200

    updates the release number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 setup(
     
     name = "sphinx_aimms_theme",
-    version = '0.1.44',
+    version = '0.1.45',
     license = "MIT",
     packages= ['sphinx_aimms_theme'],
     url = "https://github.com/aimms/sphinx-aimms-theme",
